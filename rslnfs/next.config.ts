0fc80b269773eb453f6a8f8b8795040cb1c9e133
rslnft6 <rslnft6@gmail.com>
2025-06-18 13:29:34 +0000

    حذف sw.js وتعطيل إعدادات PWA نهائياً

--- rslnfs/next.config.ts
+++ rslnfs/next.config.ts
@@ -1,15 +1,9 @@
 import type { NextConfig } from "next";
-const withPWA = require("next-pwa")({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development", // لتفادي مشاكل المتصفح البسيط أثناء التطوير
-});
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
 };
 
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
 export default nextConfig;
